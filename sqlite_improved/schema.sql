035a1811ae73e5548f2fa86c7202f96df3109c65
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-30 18:37:07 +0000

    Simplify from 3->2->1 views

--- sqlite_improved/schema.sql
+++ sqlite_improved/schema.sql
@@ -22,13 +22,14 @@ INSERT INTO neighbour_coordinates VALUES
   (+1, 0, 2),
   (+1,+1, 2);
 
-CREATE VIEW live_neighbours AS
-  SELECT simulation, generation, x+dx as xx, y+dy as yy, sum(weight) as cnt
+CREATE VIEW alive_next_generation AS
+  SELECT
+    simulation,
+    generation+1 AS generation,
+    x+dx as x,
+    y+dy as y
   FROM life
   JOIN neighbour_coordinates
-  GROUP BY simulation, generation, xx, yy
+  GROUP BY simulation, generation, life.x+dx, life.y+dy
+  HAVING SUM(weight) BETWEEN 5 and 7
 ;
-
-CREATE VIEW alive_next_generation AS
-  SELECT simulation, generation+1 as generation, xx as x, yy as y FROM live_neighbours
-  WHERE cnt BETWEEN 5 and 7;
